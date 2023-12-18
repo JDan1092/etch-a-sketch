5ebea0333e79cd5d2b68d700a05b159573848b44
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2023-12-18 16:13:25 -0600

    game completed using js

--- script.js
+++ script.js
@@ -1,13 +1,118 @@
-const container = document.getElementById("container");
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
 
-makeRows(16, 16);
+function setCurrentColor(newColor) {
+  currentColor = newColor
+}
+
+function setCurrentMode(newMode) {
+  activateButton(newMode)
+  currentMode = newMode
+}
+
+function setCurrentSize(newSize) {
+  currentSize = newSize
+}
+
+const colorPicker = document.getElementById('colorPicker')
+const colorBtn = document.getElementById('colorBtn')
+const rainbowBtn = document.getElementById('rainbowBtn')
+const deleteBtn = document.getElementById('deleteBtn')
+const clearBtn = document.getElementById('clearBtn')
+const sizeValue = document.getElementById('sizeValue')
+const sizeSlider = document.getElementById('sizeSlider')
+const grid = document.getElementById('grid')
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('rainbow')
+deleteBtn.onclick = () => setCurrentMode('delete')
+clearBtn.onclick = () => reloadGrid()
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeSize(value) {
+  setCurrentSize(value)
+  updateSizeValue(value)
+  reloadGrid()
+}
+
+function updateSizeValue(value) {
+  sizeValue.innerHTML = `${value} x ${value}`
+}
+
+function reloadGrid() {
+  clearGrid()
+  createBoard(currentSize)
+}
+
+function clearGrid() {
+  grid.innerHTML = ''
+}
+
+function createBoard(size) {
+  let grid = document.querySelector('.container')
+
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  let numDivs = size * size;
+
+  for(let i = 0; i < numDivs; i++) {
+    const gridElement = document.createElement('div')
+    gridElement.classList.add('grid-element')
+    gridElement.addEventListener('mouseover', changeColor)
+    gridElement.addEventListener('mousedown', changeColor)
+    grid.appendChild(gridElement)
+  }
+}
+
+function changeColor(e) {
+  if (e.type === 'mouseover' && !mouseDown) return
+  if (currentMode === 'rainbow') {
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+  } else if (currentMode === 'color') {
+    e.target.style.backgroundColor = currentColor
+  } else if (currentMode === 'delete') {
+    e.target.style.backgroundColor = '#fefefe'
+  }
+}
+
+function activateButton(newMode) {
+  if (currentMode === 'rainbow') {
+    rainbowBtn.classList.remove('active')
+  } else if (currentMode === 'color') {
+    colorBtn.classList.remove('active')
+  } else if(currentMode === 'delete') {
+    deleteBtn.classList.remove('active')
+  }
+
+  if (newMode === 'rainbow') {
+    rainbowBtn.classList.add('active')
+  } else if (newMode === 'color') {
+    colorBtn.classList.add('active')
+  } else if (newMode === 'delete') {
+    deleteBtn.classList.add('active')
+  }
+
+  window.onload = () => {
+    createBoard(DEFAULT_SIZE)
+    activateButton(DEFAULT_MODE)
+  }
+}
+
+
+
+
